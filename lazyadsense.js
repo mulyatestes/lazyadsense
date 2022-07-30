0a63d3a8f547311bc7613a48ae44ed17edff94f6
Mas Mul <mulya.testes@gmail.com>
2022-07-30 13:36:46 +0700

    Add files via upload

---
+++ lazyadsense.js
@@ -0,0 +1,11 @@
+<script>
+//<![CDATA[
+var lazyadsense = false;
+window.addEventListener("scroll", function(){
+if ((document.documentElement.scrollTop != 0 && lazyadsense === false) || (document.body.scrollTop != 0 && lazyadsense === false)) {
+(function() { var ad = document.createElement('script'); ad.setAttribute('crossorigin','anonymous'); ad.async = true; ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9068522252080439'; var sc = document.getElementsByTagName('script')[0]; sc.parentNode.insertBefore(ad, sc); })();
+lazyadsense = true;
+  }
+}, true);
+//]]>
+</script>
